ca07ccaba0b3e713f5b7d5102ec3b4271acdaaf5
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2023-01-21 20:30:57 +0000

    Social links

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+[![Discord](https://img.shields.io/discord/1066335615517458442?color=7289DA&label=DISCORD&logo=discord&logoColor=7289DA&style=for-the-badge)](https://discord.gg/2Ns2aNkYAS) [![Reddit](https://img.shields.io/reddit/subreddit-subscribers/explorersodyssey?color=FF4500&label=REDDIT&logo=reddit&logoColor=FF4500&style=for-the-badge)](https://www.reddit.com/r/explorersodyssey/)
 # Explorer's Odyssey
 Explorer's Odyssey is an adventure game where you must defend yourself on an endless journey.
+
+## Contributing
+See our [Contributing Guide](CONTRIBUTING.md) for information on contributing to this repository.
--- src/main/io/github/explorersodysseygame/game/common/menu/MenuScreen.java
+++ src/main/io/github/explorersodysseygame/game/common/menu/MenuScreen.java
@@ -1,16 +1,33 @@
 package io.github.explorersodysseygame.game.common.menu;
 
 import io.github.explorersodysseygame.game.Main;
+import io.github.explorersodysseygame.game.common.util.image.ImageClass;
 import io.github.explorersodysseygame.game.common.util.image.ImageReader;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicButtonUI;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Objects;
+
+import static io.github.explorersodysseygame.game.common.util.image.ImageReader.ImageMemory;
 
 public class MenuScreen extends JPanel implements ActionListener, KeyListener {
+
+    private static class SocialButtonSelectedUI extends MenuButton.MenuButtonSelectedUI {
+        SocialButtonSelectedUI(Color color) {
+            this.selectColor = Objects.requireNonNull(color);
+        }
+    }
     public static final int GRID_SIZE = 20; // Pixel size of each square on the menu grid.
     public static final int GRID_ROWS = 24; // Amount to multiply GRID_SIZE by for width.
     public static final int GRID_COLUMNS = GRID_ROWS; // Window is always square, so this is GRID_ROWS
@@ -19,16 +36,48 @@ public class MenuScreen extends JPanel implements ActionListener, KeyListener {
     private int width;
     private int height;
     public MenuScreen() {
-        reset();
-    }
-
-    public void reset() {
         int width = GRID_SIZE * GRID_ROWS - 1;
         int height = GRID_SIZE * GRID_COLUMNS;
+        Color BGCOLOUR = new Color(255, 162, 162);
         setPreferredSize(new Dimension(width, height));
-        setBackground(new Color(255, 162, 162));
+        setBackground(BGCOLOUR);
         setLayout(null);
         screen = this;
+
+        BufferedImage discord = ImageReader.read("social/discord.png");
+        BufferedImage reddit = ImageReader.read("social/reddit.png");
+
+        MenuButton discordSocial = new MenuButton("", new ImageIcon(Objects.requireNonNull(discord))) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Main.log("Pressed Discord menu button");
+                try {
+                    Desktop.getDesktop().browse(java.net.URI.create("https://discord.gg/2Ns2aNkYAS"));
+                } catch (Exception exc) {
+                    Main.log(String.format("Unable to open Discord social link: %s", exc.getMessage()));
+                }
+            }
+        };
+        discordSocial.setBackground(BGCOLOUR);
+        discordSocial.setUI(new SocialButtonSelectedUI(BGCOLOUR));
+        discordSocial.setBounds(width-37,height-37,32,32);
+        add(discordSocial);
+
+        MenuButton redditSocial = new MenuButton("", new ImageIcon(Objects.requireNonNull(reddit))) {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Main.log("Pressed Reddit menu button");
+                try {
+                    Desktop.getDesktop().browse(java.net.URI.create("https://www.reddit.com/r/explorersodyssey/"));
+                } catch (Exception exc) {
+                    Main.log(String.format("Unable to open Reddit social link: %s", exc.getMessage()));
+                }
+            }
+        };
+        redditSocial.setBackground(BGCOLOUR);
+        redditSocial.setUI(new SocialButtonSelectedUI(BGCOLOUR));
+        redditSocial.setBounds(5,height-37,32,32);
+        add(redditSocial);
     }
 
     public static MenuScreen getScreen() {
