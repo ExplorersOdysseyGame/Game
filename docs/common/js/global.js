ee93a02b364606607c16773e9552c611bb5c4ce8
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2023-02-03 20:55:56 +0000

    Storyline section

--- docs/common/js/global.js
+++ docs/common/js/global.js
@@ -3,6 +3,7 @@ function capitaliseFirstLetter(string) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+	// JS Metadata Tags
     jsdats = document.body.getElementsByTagName("jsdat")
     for (let i = 0; i < jsdats.length; i++) {
     	jsdat = jsdats[i]
@@ -11,8 +12,17 @@ document.addEventListener('DOMContentLoaded', function() {
     	jshtml = `<${jsdat.getAttribute("jname")}>${capitaliseFirstLetter(jsdat.getAttribute("jdata"))}${jsdatExtra}</${jsdat.getAttribute("jname")}>`
     	document.head.innerHTML += jshtml
     	if (jsdat.getAttribute("jhtml") != null) {
-    		document.body.innerHTML += jshtml.replace(jsdat.getAttribute("jname"), jsdat.getAttribute("jhtml")).replace(jsdatExtra, "")
+    		document.body.innerHTML = jshtml.replace(jsdat.getAttribute("jname"), jsdat.getAttribute("jhtml")).replace(jsdatExtra, "") + document.body.innerHTML
     	}
 	}
 	for (let i = 0; i < jsdats.length; i++) {jsdats[i].remove()}
+	// JS Reference Tags
+	jsrefs = document.body.getElementsByTagName("jsrel")
+	for (let i = 0; i < jsrefs.length; i++) {
+    	jsref = jsrefs[i]
+    	jsrel = jsref.getAttribute("jrel")
+    	if (jsrel == null) {jsrel = ["", ""]} else {jsrel = jsrel.split(":")}
+    	jsref.outerHTML = `<span>${jsref.getAttribute("jdata")}</span>`.replace("[]", `<a href=${jsrel[0]}>${jsrel[1]}</a>`)
+	}
+	for (let i = 0; i < jsrefs.length; i++) {jsrefs[i].remove()}
 }, false);
